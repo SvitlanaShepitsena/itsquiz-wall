cead934f229d6a2d0f8081f7d948be0e0a5ae3b9
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-01-08 14:29:11 -0600

    Mdl styles

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,7 +2,6 @@
 var User = require('../models/user');
 var local = require('./passport/local');
 var google = require('./passport/google');
-var facebook = require('./passport/facebook');
 
 
 export default function(app, passport, config) {
@@ -22,6 +21,5 @@ export default function(app, passport, config) {
 
   //use the following strategies
   passport.use(local);
-  passport.use(facebook);
   passport.use(google);
 };
--- server/config/routes.js
+++ server/config/routes.js
@@ -5,18 +5,11 @@ var _ = require('lodash');
 export default  function (app, passport) {
     // user routes
     app.post('/login', users.postLogin);
-    app.get('/logout', users.getLogout);
-
     app.post('/signup', users.postSignUp);
+    app.get('/logout', users.getLogout);
 
     app.get('/auth/google/callback',
         passport.authenticate('google', {failureRedirect: '/login'}),
-        function (req, res) {
-            // Successful authentication, redirect home.
-            res.redirect('/tutorials');
-        });
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {failureRedirect: '/login'}),
         function (req, res) {
             // Successful authentication, redirect home.
             res.redirect('/tutorials');
@@ -34,12 +27,6 @@ export default  function (app, passport) {
             'https://www.googleapis.com/auth/plus.me',
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }));
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: [
-            'public_profile',
-            'email'
         ]
     }));
 
