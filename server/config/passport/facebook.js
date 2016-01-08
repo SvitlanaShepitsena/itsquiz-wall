5e1b641cb01dcfc6f47e4dfe99e618b0412e18c0
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-08 14:16:53 -0600

    Add instructions to Readme

--- server/config/passport/facebook.js
+++ server/config/passport/facebook.js
@@ -8,46 +8,24 @@ export default new facebookStrategy({
     clientSecret: secrets.facebook.clientSecret,
     callbackURL: secrets.facebook.callbackURL,
     passReqToCallback: true,
-    profileFields: ['id', 'name','picture.type(large)', 'emails', 'username', 'displayName', 'about', 'gender']
+    profileFields: ['id', 'name', 'displayName', 'photos', 'hometown', 'profileUrl', 'friends']
 }, function (req, accessToken, refreshToken, profile, done) {
-    console.log(profile);
 
-    if (req.user) {
-        User.findOne({facebook: profile.id}, function (err, existingUser) {
-            if (existingUser) {
-                console.log(existingUser);
-                return done(null, false, {message: 'There is already a facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.'})
-            } else {
-                User.findById(req.user.id, function (err, user) {
-                    user.facebook = profile.id;
-                    user.tokens.push({kind: 'facebook', accessToken: accessToken});
-                    user.profile.name = user.profile.name || profile.displayName;
-                    user.profile.gender = user.profile.gender || profile._json.gender;
-                    user.save(function (err) {
-                        done(err, user, {message: 'facebook account has been linked.'});
-                    });
-                })
-            }
-        });
-    } else {
-        User.findOne({facebook: profile.id}, function (err, existingUser) {
-            if (existingUser) return done(null, existingUser);
-            User.findOne({email: profile._json.email}, function (err, existingEmailUser) {
-                if (existingEmailUser) {
-                    return done(null, false, {message: 'There is already an account using this email address. Sign in to that account and link it with facebook manually from Account Settings.'});
-                } else {
-                    var user = new User();
-                    user.email = profile._json.email;
-                    user.facebook = profile.id;
-                    user.tokens.push({kind: 'facebook', accessToken: accessToken});
-                    user.profile.name = profile._json.displayName;
-                    user.profile.gender = profile._json.gender;
-                    user.profile.picture = profile._json.image.url;
-                    user.save(function (err) {
-                        done(err, user);
-                    });
-                }
-            });
-        });
-    }
+    User.findOne({facebook: profile.id}, function (err, existingUser) {
+        if (existingUser) {
+            return done(null, existingUser);
+        } else {
+            User.findById(profile.id, function (err, user) {
+                user.facebook = profile.id;
+                //user.email = profile.email;
+                user.tokens.push({kind: 'facebook', accessToken: accessToken});
+                user.profile.name = profile.displayName;
+                user.profile.gender = profile.gender;
+                user.profile.picture = profile.photos[0].value;
+                user.save(function (err) {
+                    done(err, user, {message: 'facebook account has been linked.'});
+                });
+            })
+        }
+    });
 });
