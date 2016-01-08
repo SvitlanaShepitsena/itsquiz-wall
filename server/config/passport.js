978ba2c7d9c95d932f72c9b3a86ed13dcc7616bf
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-08 14:31:28 -0600

    Facebook works

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,7 @@
 var User = require('../models/user');
 var local = require('./passport/local');
 var google = require('./passport/google');
+var facebook = require('./passport/facebook');
 
 
 export default function(app, passport, config) {
@@ -21,5 +22,6 @@ export default function(app, passport, config) {
 
   //use the following strategies
   passport.use(local);
+  passport.use(facebook);
   passport.use(google);
 };
