f7da6a29a6a7403ca083c892eb1b93a74ef25412
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-01-08 21:49:48 -0600

    Single Article Page is cleaned

--- shared/components/pages/ArticlePage.jsx
+++ shared/components/pages/ArticlePage.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import cx    from 'classnames';
 
-import { Card, CardTitle, CardActions } from 'react-mdl/lib/Card';
+import { Card } from 'react-mdl/lib/Card';
 import Grid, { Cell }                   from 'react-mdl/lib/Grid';
-import Button                           from 'react-mdl/lib/Button';
-import IconButton                       from 'react-mdl/lib/IconButton';
 import Spinner                          from 'react-mdl/lib/Spinner';
 
-import Icon                 from '../Icon.jsx';
 import LoginDialog          from '../../containers/LoginDialog.jsx';
-import AppBarWithBackground from '../AppBarWithBackground.jsx';
 
 import { sprintf } from '../../utils';
 
@@ -31,27 +27,12 @@ export default class ArticlePage extends React.Component {
             article,
             authorArticles,
             isLoading,
-            onPass,
-            onShare,
-            onShareResult,
-            onStopSharing,
-            onSponsoredClick,
-            onArticleClick,
-            onViewAnswers
             } = this.props;
 
         const {
             pictureURL,
-            name,
-            isPrivate,
-            userQuizSession,
-            numberOfQuestions,
-            timeToPass,
             author,
-            isSponsored
             } = article;
-
-        const {l, ngettext, humanizeDuration} = this.context.i18n;
 
         if (isLoading) {
             return <Spinner className='ArticlePage__spinner'/>;
@@ -62,7 +43,7 @@ export default class ArticlePage extends React.Component {
                 <Card className='ArticlePage__paper' shadow={1}>
                     <div className='ArticlePage__details'>
                         <p className='ArticlePage__message'>
-                            At vero eos et accusamus et iusto odio dignissimos ducimus
+                            Here is a article.
                         </p>
                     </div>
                 </Card>
@@ -86,17 +67,11 @@ export default class ArticlePage extends React.Component {
     }
 
     render() {
-        const { l } = this.context.i18n;
         const {
             article,
-            sharingLink,
             isLoading,
             isLoggingIn,
-            isEmbedded,
-            userQuizSession,
-            onShare,
             onLoginDialogClose,
-            onStopSharing,
             onGoBack
             } = this.props;
 
@@ -104,33 +79,16 @@ export default class ArticlePage extends React.Component {
             'ArticlePage--loading': isLoading,
         });
 
-        console.log(userQuizSession);
-
         return (
             <div className={classes}>
                 <LoginDialog
                     isOpen={isLoggingIn}
                     onRequestClose={onLoginDialogClose}
                 />
-
                 <div className='ArticlePage__content'>
                     {this.renderContent()}
                 </div>
             </div>
         );
-    }
-
-    getGreetingPhrase = (grade) => {
-        const { l } = this.context.i18n;
-
-        const phrases = {
-            'verybad': l('You could do better!'),
-            'bad': l('You could do better!'),
-            'normal': l('Good job!'),
-            'good': l('Great result!'),
-            'excellent': l('You rock! Excellent job!')
-        };
-
-        return phrases[grade];
     }
 }
